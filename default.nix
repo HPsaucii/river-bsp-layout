dc7d336d2ed81d75a8a6a005a554754e406a1596
HPsaucii <me@hpsaucii.dev>
2024-07-28 12:54:03 +0100

    update hash

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ rustPlatform.buildRustPackage {
     owner = "areif-dev";
     repo = "river-bsp-layout";
     rev = "8ca2dd6e5d5f21e6dbef8900a25f3389e2061e8f";
-    sha256 = "";
+    sha256 = "sha256-DYgJ+bqfJfX3AuH+clQCYCvaRxGsTou3bSVlgkGFlRA=";
   };
   cargoLock.lockFile = ./Cargo.lock;
 }
